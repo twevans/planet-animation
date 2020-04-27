45bd749c19a1b804beab99008bc84a651cd90668
Tom Evans <twevans100@yahoo.co.uk>
2020-04-27 21:17:41 +0100

    add classes for calculations and paramter values

---
+++ Calculations.java
@@ -0,0 +1,39 @@
+package animation;
+
+import java.util.Calendar;
+
+public class Calculations extends ParameterValues {
+
+	public final double x(double rA){
+    	double x = (chartWidth-rAMult*rA/hourDeg)/chartWidth;
+    	return x;
+    }
+    
+    public final double y(double dec){
+    	double y = ((chartHeight/2)-decMult*dec)/chartHeight;
+    	return y;
+    }
+    
+    public final double r(double m, double w, double h){
+    	double r = m*Math.sqrt((Math.pow(w,2)+Math.pow(h,2))/(Math.pow(chartWidth,2)+Math.pow(chartHeight,2)));
+    	return r;
+    }
+    
+    public final double rStar(double m, double w, double h){
+        	double r = m*Math.sqrt((Math.pow(w,2)+Math.pow(h,2))/(Math.pow(chartWidth1,2)+Math.pow(chartHeight1,2)));
+        	return r;
+    }
+    
+    public final double d(Calendar c){
+    	int year   = c.get(Calendar.YEAR);
+        int month  = c.get(Calendar.MONTH) + 1;
+        int day    = c.get(Calendar.DAY_OF_MONTH);
+        int hour   = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        int dInt   = 367*year - 7*(year+(month+9)/12)/4 + 275*month/9 + day - 730530;
+        double d   = dInt + (hour + minute/60.0)/24.0;
+        return d;
+    }
+	
+	
+}
---
+++ ParameterValues.java
@@ -0,0 +1,23 @@
+package animation;
+
+public class ParameterValues {
+	
+	public static final int chartWidth = 1080; 
+	public static final int chartHeight = 500;
+	public static final int rAMult = 45;
+	public static final int decMult = 3;
+	public static final int mSun = 8;
+	public static final int mMoon = 8;
+	public static final int mPlanet = 2;
+	public static final int mEcliptic = 1;
+	public static final int hourDeg = 15;
+	public static final int fontSize = 15;
+	public static final int pFontSize = 12;
+	
+	public static double mStar = 0.5;
+	public static double chartWidth1 = 1080; 
+	public static double chartHeight1 = 500;
+	
+    
+
+}
